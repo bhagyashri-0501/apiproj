3674d9de4b67997026bea1c8cf0690245f8c0fb0
NTS-BHAGYASHRIK\NTS-BhagyashriK <bhagyashri.kalaskar@neutrinotechlabs.com>
2022-06-11 13:24:37 +0530

    latest changes

--- src/test/java/api/getdata.java
+++ src/test/java/api/getdata.java
@@ -1,5 +1,8 @@
 package api;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.testng.Assert;
@@ -16,11 +19,13 @@ public class getdata {
     @BeforeClass
     public void startUp() throws IOException {
         APIUtils.readAPIConfigs();
-      //  APIUtils.setBaseURL();
     }
 
-    @Test(priority=1)
-     public void getallusersinfo()
+    @Feature("API Test")
+    @Story("API-001: api test for get users functionality")
+    @Description("to test new user is created and get info of created of user  when create user api is called")
+    @Test(description = "to verify new user is created when create user api is called")
+    public void get_all_users_data_using_GET_Api()
     {
         Response response=
         given()
@@ -30,28 +35,12 @@ public class getdata {
         response.then()
         .statusCode(200);
 
-      //  response.body().asString();
         System.out.println(response.body().asString());
 
-//        Response response = RestAssured.get("base_url1");
-//        System.out.println(response.asString());
-//        System.out.println(response.getBody().asString());
-//        System.out.println(response.getStatusCode());
-//        System.out.println(response.getHeader("content-type"));
-//        System.out.println(response.getTime());
-
-        //int statuscode = response.getStatusCode();
-      //  Assert.assertEquals(statuscode,200);
     }
-
-
-
-
-
-
 }
 
-//https://reqres.in/api/users?page=2
+
 
 
 
--- src/test/java/api/postdata.java
+++ src/test/java/api/postdata.java
@@ -1,57 +1,50 @@
 package api;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import io.restassured.http.ContentType;
-import org.json.JSONObject;
+import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import utilities.APIUtils;
-
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.Map;
-
 import static io.restassured.RestAssured.given;
 
 public class postdata {
-
     @BeforeClass
     public void startUp() throws IOException {
         APIUtils.readAPIConfigs();
         APIUtils.setBaseURL2();
     }
 
-    @Test(priority =2)
+    @Feature("API Test")
+    @Story("API-001: api test for create user functionality")
+    @Description("to test new user is created when create user api is called")
+    @Test(description = "to verify new user is created when create user api is called")
     public void test_user_created_successfully_using_post_api_method1_body_from_json_file() throws Exception {
         String end_point = APIUtils.api_config.getProperty("create_user_end_point");
         String body = Files.readString(Path.of(System.getProperty("user.dir") + "/src/test/java/resources/api_test_data_create_userJSONBody.json"));
 
-//        Map<String, Object> map = new HashMap<String, Object>();
-  //      map.put("name", "Raghav");
-    //    map.put("job", "Teacher");
-    //    System.out.println(map);
-
-//        JSONObject request = new JSONObject();
-//        request.put("name", "shri");
-//        request.put("job", "Teacher");
-//        System.out.println(request.toString());
 
          String baseURI = "base_url2";
 
-        given().
+        Response response= given().
                 header("Content-Type", "application/json").
                 contentType(ContentType.JSON).
                 accept(ContentType.JSON).
                 body(body).
                 when().
-                post(baseURI+"/users").
-                then().
-                statusCode(201).log().all();
+                post(baseURI+"/users");
 
-      //  int statuscode = response.getStatusCode();
-      //  Assert.assertEquals(statuscode,201);
+               response.then()
+                .statusCode(201).log().all();
+
+        System.out.println(response.body().asString());
+
+
     }
 }
